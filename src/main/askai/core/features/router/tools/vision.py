c05692fced01f62bec48fc3f4691edb035bda35b
Hugo Saporetti Junior <yorevs@gmail.com>
2024-08-23 16:41:24 -0300

    Add the webcam_capturer tool - Done

--- src/main/askai/core/features/router/tools/vision.py
+++ src/main/askai/core/features/router/tools/vision.py
@@ -7,6 +7,7 @@ from askai.core.askai_messages import msg
 from askai.core.features.validation.accuracy import resolve_x_refs
 from askai.core.support.shared_instances import shared
 from hspylib.core.config.path_object import PathObject
+from hspylib.core.enums.enumeration import Enumeration
 from transformers import BlipForConditionalGeneration, BlipProcessor
 
 
@@ -14,7 +15,21 @@ def image_captioner(path_name: str) -> Optional[str]:
     """This tool is used to describe an image.
     :param path_name: The path of the image to describe.
     """
-    caption: str | None = None
+
+    class HFModel(Enumeration):
+        """Available Hugging Face models"""
+        # fmt: off
+        SF_BLIP_BASE            = "Salesforce/blip-image-captioning-base"
+        SF_BLIP_LARGE           = "Salesforce/blip-image-captioning-large"
+        # fmt: on
+
+        @staticmethod
+        def default() -> 'HFModel':
+            """Return the default HF model."""
+            return HFModel.SF_BLIP_LARGE
+
+    caption: str = 'Not available'
+
     posix_path: PathObject = PathObject.of(path_name)
     if not posix_path.exists:
         # Attempt to resolve cross-references
@@ -25,21 +40,15 @@ def image_captioner(path_name: str) -> Optional[str]:
 
     if posix_path.exists:
         events.reply.emit(message=msg.describe_image(str(posix_path)))
-        # specify model to be used
-        hf_model = "Salesforce/blip-image-captioning-base"
-        # use GPU if it's available
+        hf_model: HFModel = HFModel.default()
+        # Use GPU if it's available
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        # preprocessor will prepare images for the model
-        processor = BlipProcessor.from_pretrained(hf_model)
-        # then we initialize the model itself
-        model = BlipForConditionalGeneration.from_pretrained(hf_model).to(device)
-        # download the image and convert to PIL object
         image = Image.open(str(posix_path)).convert("RGB")
-        inputs = processor(image, return_tensors="pt").to(device)
-        # generate the caption
-        out = model.generate(**inputs, max_new_tokens=20)
-        # get the caption
-        caption = processor.decode(out[0], skip_special_tokens=True)
+        model = BlipForConditionalGeneration.from_pretrained(hf_model.value).to(device)
+        processor = BlipProcessor.from_pretrained(hf_model.value)
+        inputs = processor(images=image, return_tensors="pt").to(device)
+        outputs = model.generate(**inputs)
+        caption = processor.decode(outputs[0], skip_special_tokens=True)
         caption = caption.title() if caption else 'I could not caption the image'
 
     return caption
--- src/main/askai/core/features/router/tools/webcam.py
+++ src/main/askai/core/features/router/tools/webcam.py
@@ -14,22 +14,25 @@ def webcam_capturer(photo_name: str | None, detect_faces: bool = False) -> str:
 
     pic_file, pic_data = camera.capture(photo_name)
     face_desc: str | None = None
+    ln: str = os.linesep
 
     if detect_faces:
         face_files, face_datas = camera.detect_faces(pic_data, photo_name)
         faces: int = len(face_files)
         face_desc = dedent(
-            f"3. Faces Detected {faces}: \n" +
+            f"3. Faces Detected ({faces}): \n" +
             indent(
-                f"- {'- '.join([ff.img_path + os.linesep for ff in face_files])}", '    ') +
-            f"4. Face Captions {faces}: \n" +
+                f"- {'- '.join([ff.img_path + ln for ff in face_files])}", '    ') +
+            f"4. Face Captions ({faces}): \n" +
             indent(
-                f"- {'- '.join([basename(ff.img_path) + ': ' + ff.img_caption + os.linesep for ff in face_files])}", '    ')
-        )
+                f"- {'- '.join([basename(ff.img_path) + ': ' + ff.img_caption + ln for ff in face_files])}", '    ')
+        ).strip()
 
-    return dedent(
-        f"> Photography Taken\n\n"
+    caption = dedent(
+        f">   Photo Taken\n\n"
         f"1. Location: {pic_file.img_path}\n"
         f"2. Caption: {pic_file.img_caption}\n"
         f"{face_desc}").strip()
+
+    return caption
 
