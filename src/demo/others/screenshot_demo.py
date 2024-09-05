be5a9c3014d622859f8fd677482818fbff2fcf1e
Hugo Saporetti Junior <yorevs@gmail.com>
2024-09-05 02:32:55 -0300

    Add screenshot demo

---
+++ src/demo/others/screenshot_demo.py
@@ -0,0 +1,18 @@
+import pyautogui
+
+
+def take_screenshot(file_path):
+    # Take a screenshot using pyautogui
+    screenshot = pyautogui.screenshot()
+
+    # Check if saving as JPEG and convert to RGB if necessary
+    if file_path.lower().endswith('.jpg') or file_path.lower().endswith('.jpeg'):
+        screenshot = screenshot.convert('RGB')
+
+    # Save the screenshot to the specified file path
+    screenshot.save(file_path)
+    print(f"Screenshot saved at {file_path}")
+
+
+if __name__ == "__main__":
+    take_screenshot('gabiroba.jpeg')
