458e6121a256e138f51762481f5236977e9e0064
Hugo Saporetti Junior <yorevs@gmail.com>
2024-10-03 00:14:08 -0300

    Include resources in manifest

---
+++ src/demo/devel/google_suggestions.py
@@ -0,0 +1,16 @@
+import json
+import requests
+
+
+if __name__ == '__main__':
+    query = 'home'
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0"
+    }
+
+    response = requests.get(f"http://google.com/complete/search?client=chrome&q={query}")
+
+    for completion in json.loads(response.text)[1]:
+        print(completion)
+
