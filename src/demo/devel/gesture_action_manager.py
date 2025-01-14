9e31e5b4fe8d0b134bb58a848d0df52afe246134
Hugo Saporetti Junior <yorevs@gmail.com>
2025-01-14 20:26:17 -0300

    Improved action manager

--- src/demo/devel/gesture_action_manager.py
+++ src/demo/devel/gesture_action_manager.py
@@ -1,9 +1,10 @@
+import random
 import threading
-from functools import cached_property
+from functools import cached_property, lru_cache
 from os.path import dirname
 from pathlib import Path
 import os
-from typing import TypeAlias, Sequence
+from typing import TypeAlias, Sequence, Any
 
 import cv2
 import numpy as np
@@ -14,6 +15,7 @@ from mediapipe.python.solutions.face_mesh_connections import FACEMESH_TESSELATIO
 from mediapipe.python.solutions.hands_connections import HAND_CONNECTIONS
 from mediapipe.python.solutions.holistic import Holistic
 from mediapipe.python.solutions.pose_connections import POSE_CONNECTIONS
+from matplotlib import pyplot as plt
 from numpy import ndarray
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
@@ -23,15 +25,15 @@ from tensorflow.keras.utils import to_categorical
 
 from askai.core.component.audio_player import player
 
-DATA_PATH: Path = Path(os.path.join(dirname(__file__), 'MP_Data'))
+DATA_PATH: Path = Path(os.path.join(dirname(__file__), 'mp-data'))
 if not DATA_PATH.exists():
     DATA_PATH.mkdir(parents=True, exist_ok=True)
 
-LOGS_PATH: Path = Path(os.path.join(dirname(__file__), 'Logs'))
+LOGS_PATH: Path = Path(os.path.join(dirname(__file__), 'logs'))
 if not LOGS_PATH.exists():
     LOGS_PATH.mkdir(parents=True, exist_ok=True)
 
-MODEL_PATH: Path = Path(os.path.join(dirname(__file__), 'Models'))
+MODEL_PATH: Path = Path(os.path.join(dirname(__file__), 'models'))
 if not MODEL_PATH.exists():
     MODEL_PATH.mkdir(parents=True, exist_ok=True)
 
@@ -162,7 +164,7 @@ class GestureActionManager:
 
     def __init__(self):
         self._actions = np.array([
-            'maximize', #'minimize', 'grab'  # , 'pointer', 'click', 'next', 'previous', 'dispose'
+            'maximize', 'minimize', 'grab', 'click',  # 'pointer', 'next', 'previous', 'dispose'
         ])
         self._tb_callback = TensorBoard(log_dir=LOGS_PATH)
 
@@ -171,13 +173,15 @@ class GestureActionManager:
         return self._actions
 
     @cached_property
-    def label_map(self) -> dict[str, int]:
+    def label_map(self) -> dict[tuple[int, Any], int]:
         return {label: num for num, label in enumerate(self._actions)}
 
-    def capture_landmarks(self):
+    def capture_landmarks(self, start_at: str = 'maximize', end_at: str = 'dispose') -> None:
         cap = cv2.VideoCapture(0)
         with Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-            for action in self.actions:
+            start_index = np.where(self.actions == start_at)[0][0]
+            end_index = np.where(self.actions == end_at)[0][0]
+            for action in self.actions[start_index:end_index + 1]:
                 for video_num in range(self.NUM_VIDEOS):
                     for frame_num in range(self.NUM_FRAMES):
                         self.setup_folder(action, video_num)
@@ -237,12 +241,13 @@ class GestureActionManager:
         cap.release()
         cv2.destroyAllWindows()
 
-    def train_model(self, epochs: int = 2000) -> None:
+    def train_model(self, epochs: int = 1000, load: bool = False) -> Sequential:
         print("Training model...")
         sequences, labels = self.create_sequences_and_labels()
         x_arr = np.array(sequences)
         y_arr = to_categorical(labels).astype(int)
         x_train, x_test, y_train, y_test = train_test_split(x_arr, y_arr, test_size=0.05)
+        model_path: str = os.path.join(MODEL_PATH, f"taius-{'-'.join(self.actions)}-gestures.h5")
         model = Sequential()
         model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(self.NUM_FRAMES, self.NUM_KEYPOINTS)))
         model.add(LSTM(128, return_sequences=True, activation='relu'))
@@ -250,15 +255,82 @@ class GestureActionManager:
         model.add(Dense(64, activation='relu'))
         model.add(Dense(32, activation='relu'))
         model.add(Dense(self.actions.shape[0], activation='softmax'))
-        model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-        model.fit(x_train, y_train, epochs=epochs, callbacks=[self._tb_callback])
+        if not load:
+            model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+            model.fit(x_train, y_train, epochs=epochs, callbacks=[self._tb_callback])
+            model.save(model_path)
+        else:
+            model.load_weights(model_path)
         model.summary()
         print("Model is READY!")
-        model.save(os.path.join(MODEL_PATH, f"taius-{'-'.join(self.actions)}-gestures.h5"))
+        return model
+
+    @lru_cache
+    def rrgb(self, _: int) -> tuple[int, ...]:
+        return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
+    def prob_vis(self, results, input_frame) -> Any:
+        colors = [
+            self.rrgb(n) for n in range(len(self.actions))
+        ]
+        output_frame = input_frame.copy()
+        for num, prob in enumerate(results):
+            self.display(
+                output_frame, str(self.actions[num]), (0, 85 + num * 40), (255, 255, 255))
+            if prob >= 0.95:
+                print('Action:', str(self.actions[num]))
+            cv2.rectangle(
+                output_frame, (0, 60 + num * 40), (int(prob * 100), 90 + num * 40), colors[num], -1)
+
+        return output_frame
+
+    def predict(self, model: Sequential) -> None:
+        sequence = []
+        actions = []
+        predictions = []
+        threshold = 0.5
+        cap = cv2.VideoCapture(0)
+
+        with Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+            while cap.isOpened():
+                ret, frame = cap.read()
+                image, results = self.mediapipe_detection(frame, holistic)
+                self.draw_styled_landmarks(image, results)
+
+                keypoints = self.extract_keypoints(results)
+                sequence.append(keypoints)
+                sequence = sequence[-self.NUM_FRAMES:]  # Get the latest sequence.
+
+                if len(sequence) == self.NUM_FRAMES:  # Number of frames to be able to predict.
+                    results = model.predict(np.expand_dims(sequence, axis=0))[0]
+                    predictions.append(np.argmax(results))  # contains 1 for the detected action: [0, 1, 0] -> minimize
+                    if np.unique(predictions[-10:])[0] == np.argmax(results):
+                        if results[np.argmax(results)] > threshold:
+                            if len(actions) > 0:
+                                if self.actions[np.argmax(results)] != actions[-1]:
+                                    actions.append(self.actions[np.argmax(results)])
+                            else:
+                                actions.append(self.actions[np.argmax(results)])
+                    if len(actions) > 5:
+                        actions = actions[-5:]
+
+                    # Viz probabilities
+                    image = self.prob_vis(results, image)
+
+                cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
+                self.display(image, ' '.join(actions), (3, 30), (255, 255, 255))
+                cv2.imshow('OpenCV Feed', image)
+
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
+
+            cap.release()
+            cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
     gm: GestureActionManager = GestureActionManager()
     # gm.demo_media_pipe()
-    # gm.capture_landmarks()
-    gm.train_model(10)
+    # gm.capture_landmarks(start_at='dispose', end_at='dispose')
+    # gm.predict(gm.train_model(250))
+    gm.predict(gm.train_model(load=True))
